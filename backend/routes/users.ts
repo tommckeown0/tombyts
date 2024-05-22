c51a667724e7c6219b27e43fe2e76e358bed5ed9
tom <tommckeown909@gmail.com>
2024-05-22 14:21:22 +0100

    Add user object and route to back end. We can create users with API requests now

---
+++ backend/routes/users.ts
@@ -0,0 +1,31 @@
+import express, { Request, Response } from "express";
+import bcrypt from "bcrypt";
+import User, { IUser } from "../models/user";
+
+const router = express.Router();
+
+// Create a new user
+router.post("/", async (req: Request, res: Response) => {
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
+
+    const user: IUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        passwordHash: hashedPassword,
+    });
+
+    try {
+        const newUser = await user.save();
+        res.status(201).json(newUser);
+    } catch (error: unknown) {
+        // Type guard to check if error is an instance of Error
+        if (error instanceof Error) {
+            res.status(400).json({ message: error.message });
+        } else {
+            res.status(400).json({ message: "An unknown error occurred" });
+        }
+    }
+});
+
+export default router;
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import connectDB from "../config/database";
 import movieRoutes from "../routes/movies";
+import userRoutes from "../routes/users";
 const cors = require("cors");
 
 const app = express();
@@ -13,6 +14,7 @@ connectDB();
 app.use(express.json());
 
 app.use("/movies", movieRoutes);
+app.use("/users", userRoutes);
 app.use("/media", express.static("C:\\Users\\tommc\\Documents\\Torrents"));
 
 app.get("/", (req, res) => {
