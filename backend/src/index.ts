ef25dffa300b764c072b01ab7af0b3e8fba2c7fe
tom <tommckeown909@gmail.com>
2024-05-22 18:45:06 +0100

    Remove weird tomdesktop index file that came out of nowhere

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,10 @@ import express from "express";
 import connectDB from "../config/database";
 import movieRoutes from "../routes/movies";
 import userRoutes from "../routes/users";
+import loginRoutes from "../routes/auth";
+import dotenv from "dotenv";
+dotenv.config();
+
 const cors = require("cors");
 
 const app = express();
@@ -15,7 +19,11 @@ app.use(express.json());
 
 app.use("/movies", movieRoutes);
 app.use("/users", userRoutes);
-app.use("/media", express.static("C:\\Users\\tommc\\Documents\\Torrents"));
+app.use(
+    "/media",
+    express.static("C:\\Users\\tommc\\OneDrive\\Documents\\Torrents")
+);
+app.use("/auth", loginRoutes);
 
 app.get("/", (req, res) => {
     res.send("Hello from TypeScript and Express!");
